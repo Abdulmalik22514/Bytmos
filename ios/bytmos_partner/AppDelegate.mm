bcf1bd239b82d3aa8de2704abb5a7a470411cc2f
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-06-25 18:57:46 +0100

    Configure SplashScreen setup for ios

--- ios/bytmos_partner/AppDelegate.mm
+++ ios/bytmos_partner/AppDelegate.mm
@@ -3,6 +3,8 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNSplashScreen.h"  // here
+
 
 #import <React/RCTAppSetupUtils.h>
 
@@ -54,6 +56,8 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];  // here
+
   return YES;
 }
 
