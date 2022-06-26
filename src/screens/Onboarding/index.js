994d3fb7b690fcf0700042dc027b777089d76b14
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-06-26 21:51:02 +0100

    Refactor

--- src/screens/Onboarding/index.js
+++ src/screens/Onboarding/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import AppIntroSlider from 'react-native-app-intro-slider';
-import {COLORS, FONTS, SIZES} from '../../constants/theme';
+import {COLORS} from '../../constants/theme';
 import {GetStartedArrow} from '../../assets/svgs/svg';
 import Slide1 from './slide1';
 import Slide2 from './slide2';
 import Slide3 from './slide3';
+import {OnboardingStyles as styles} from './styles';
 
 const slides = [
   {
@@ -60,37 +61,3 @@ export default function Onboarding({navigation}) {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: COLORS.white,
-    paddingTop: SIZES.font9 * 3,
-    paddingHorizontal: SIZES.font7,
-    paddingBottom: SIZES.font10 * 4,
-  },
-  dotStyle: {
-    borderColor: COLORS.blue,
-    width: SIZES.font10,
-    height: SIZES.font10,
-    borderRadius: 10,
-    borderWidth: 1,
-    marginRight: SIZES.font5,
-    marginBottom: SIZES.font1,
-  },
-  doneButtonStyle: {
-    width: SIZES.font1 * 5.2,
-    height: SIZES.font1 * 1.5,
-    flexDirection: 'row',
-    backgroundColor: COLORS.blue,
-    alignItems: 'center',
-    marginTop: SIZES.font2,
-    borderRadius: 15,
-    justifyContent: 'space-evenly',
-    marginRight: SIZES.font10 * 0.01,
-  },
-  getStartedText: {
-    ...FONTS.h10,
-    color: COLORS.white,
-  },
-});
