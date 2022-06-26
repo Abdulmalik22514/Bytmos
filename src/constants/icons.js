bf51fe43aaaffdbb5e0f0ba448c41cd4327c2e6f
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-06-26 21:51:27 +0100

    Add svg and icons

---
+++ src/constants/icons.js
@@ -0,0 +1,11 @@
+const SplashIcon = require('../assets/images/bytmosPartner.png');
+const Onboarding1 = require('../assets/images/slide1.png');
+const Onboarding2 = require('../assets/images/slide2.png');
+const Onboarding3 = require('../assets/images/slide3.png');
+
+export default {
+  SplashIcon,
+  Onboarding1,
+  Onboarding2,
+  Onboarding3,
+};
