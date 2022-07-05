5dbacb7127fe8ae30e2bd1833a2f4b8bc22ad362
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-07-05 18:55:14 +0100

    Add icons and svg

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -3,6 +3,13 @@ const Onboarding1 = require('../assets/images/slide1.png');
 const Onboarding2 = require('../assets/images/slide2.png');
 const Onboarding3 = require('../assets/images/slide3.png');
 const BlueLogo = require('../assets/images/blueLogo.png');
+const Verify_Otp = require('../assets/images/verify_otp.png');
+const Success = require('../assets/images/success.png');
+const Home = require('../assets/images/home.png');
+const Tribe = require('../assets/images/tribe.png');
+const Envelope = require('../assets/images/envelope.png');
+const Wallet = require('../assets/images/wallet.png');
+const Profile = require('../assets/images/profile.png');
 
 export default {
   SplashIcon,
@@ -10,4 +17,11 @@ export default {
   Onboarding2,
   Onboarding3,
   BlueLogo,
+  Verify_Otp,
+  Success,
+  Home,
+  Tribe,
+  Envelope,
+  Wallet,
+  Profile,
 };
