99f699fe28121e4c59e3c11de041625e70af5d16
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-07-07 23:13:42 +0100

    Refactor: Add colors

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -4,9 +4,9 @@ const {width, height} = Dimensions.get('window');
 
 export const COLORS = {
   // primary colors
-  primary: '#1648CE',
-  secondary: '#929CAD',
-  tertiary: '#FCF9FF',
+  red: '#CA5353',
+  verify: '#4E4F50',
+  background: '#F3F3F3',
   white: '#FFFFFF',
   grey: '#C7C7C7',
   black: '#000000',
@@ -14,6 +14,7 @@ export const COLORS = {
   blue: '#00709E',
   separator: '#D3D3D3',
   input: '#828181',
+  otp: '#A4A2A2',
 };
 export const SIZES = {
   // font sizes
@@ -33,22 +34,30 @@ export const SIZES = {
   width,
   height,
 };
+
 export const FONTS = {
   largeTitle: {
     fontFamily: 'Roboto-Bold',
     fontSize: SIZES.largeTitle,
     lineHeight: 30.47,
   },
-  h1: {fontFamily: 'Roboto-Bold', fontSize: SIZES.font1, lineHeight: 29.3},
+  h1: {
+    fontFamily: 'Roboto-Bold',
+    fontSize: SIZES.font1,
+    lineHeight: 29.3,
+  },
   h2: {fontFamily: 'Roboto-Bold', fontSize: SIZES.font2, lineHeight: 28.13},
   h3: {fontFamily: 'Roboto-Bold', fontSize: SIZES.font3, lineHeight: 25.78},
   h4: {fontFamily: 'Roboto-Bold', fontSize: SIZES.font5, lineHeight: 23},
   h5: {fontFamily: 'Roboto-Medium', fontSize: SIZES.font3, lineHeight: 25.78},
   h6: {fontFamily: 'Roboto-Medium', fontSize: SIZES.font7, lineHeight: 21.09},
-  h7: {fontFamily: 'Roboto-Medium', fontSize: SIZES.font2, lineHeight: 28.13},
+  h7: {fontFamily: 'Roboto-Medium', fontSize: SIZES.font4},
   h8: {fontFamily: 'Roboto-Medium', fontSize: SIZES.font6, lineHeight: 22.27},
   h9: {fontFamily: 'Roboto-Medium', fontSize: SIZES.font7, lineHeight: 22.27},
-  h10: {fontFamily: 'Roboto-Bold', fontSize: SIZES.font6, lineHeight: 25.78},
+  h10: {
+    fontFamily: 'Roboto-Bold',
+    fontSize: SIZES.font6,
+  },
 
   body1: {
     fontFamily: 'Roboto-Regular',
