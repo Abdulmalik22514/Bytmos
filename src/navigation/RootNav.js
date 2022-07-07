81c4deee4f352dd111d4aac7d604f6f1ad4becce
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-07-07 23:14:00 +0100

    Add screens

--- src/navigation/RootNav.js
+++ src/navigation/RootNav.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Login from '../screens/Login';
-import Onboarding from '../screens/Onboarding';
 import {StatusBar} from 'react-native';
-import {COLORS} from '../constants/theme';
 import NotchResponsive from '../components/NotchResponsive';
-import TermsAndPrivacy from '../screens/TermsPrivacy';
-import SignUp from '../screens/SignUp';
-import VerifyOtp from '../screens/VerifyOtp';
+import VerifyOtp from '../screens/VerifyOtp/VerifyOtp';
+import Login from '../screens/Login/Login';
+import Onboarding from '../screens/Onboarding/Onboarding';
+import TermsAndPrivacy from '../screens/TermsPrivacy/TermsPrivacy';
+import SignUp from '../screens/SignUp/SignUp';
+import BottomTabs from './TabNav';
 
 const Stack = createNativeStackNavigator();
 
@@ -22,11 +22,11 @@ function RootNavigator() {
           headerShown: false,
         }}>
         <Stack.Screen name="Onboarding" component={Onboarding} />
-
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="TermsPrivacy" component={TermsAndPrivacy} />
         <Stack.Screen name="SignUp" component={SignUp} />
         <Stack.Screen name="VerifyOtp" component={VerifyOtp} />
+        <Stack.Screen name="BottomTab" component={BottomTabs} />
       </Stack.Navigator>
     </NavigationContainer>
   );
