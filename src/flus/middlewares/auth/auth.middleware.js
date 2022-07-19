ba7576dda613b5b6a8d701087c2f73553fb59bbb
Obi Pascal <pascalobi83@gmail.com>
2022-07-19 14:27:40 +0100

    first build

--- src/flus/middlewares/auth/auth.middleware.js
+++ src/flus/middlewares/auth/auth.middleware.js
@@ -1,45 +1,47 @@
-import {saveUser, setIsActive, setToken} from '../../../services/authServices';
+import {destorySession, saveUser, setIsActive, setToken} from '../../../services/authServices'
 
 export const AuthMiddleware = {
-  auth: {
-    login(state, payload) {
-      /* perform the state logic here! */
-      state.auth.isActive = true;
-      state.auth.user = payload?.user;
-      state.auth.api_token = payload?.access_token;
-      state.auth.app.hasOnboarded = true;
-      state.auth.app.termsAccepted = true;
+	auth: {
+		login(state, payload) {
+			/* perform the state logic here! */
+			state.auth.isActive = true
+			state.auth.user = payload?.user
+			state.auth.api_token = payload?.access_token
+			state.auth.app.hasOnboarded = true
+			state.auth.app.termsAccepted = true
 
-      setToken(payload?.access_token ? payload?.access_token : '');
-      saveUser(payload?.user);
-      setIsActive();
+			setToken(payload?.access_token ? payload?.access_token : '')
+			saveUser(payload?.user)
+			setIsActive()
 
-      /* return the new state data */
-      return {
-        ...state,
-      };
-    },
+			/* return the new state data */
+			return {
+				...state,
+			}
+		},
 
-    setApiToken(state, payload) {
-      /* perform the state logic here! */
-      state.auth.api_token = payload?.token;
+		setApiToken(state, payload) {
+			/* perform the state logic here! */
+			state.auth.api_token = payload?.token
 
-      setToken(payload?.token ? payload?.token : '');
+			setToken(payload?.token ? payload?.token : '')
 
-      /* return the new state data */
-      return {
-        ...state,
-      };
-    },
+			/* return the new state data */
+			return {
+				...state,
+			}
+		},
 
-    logout(state) {
-      state.auth.isActive = false;
-      state.auth.api_token = null;
-      state.auth.user = null;
+		logout(state) {
+			destorySession()
 
-      return {
-        ...state,
-      };
-    },
-  },
-};
+			state.auth.isActive = false
+			state.auth.api_token = null
+			state.auth.user = null
+
+			return {
+				...state,
+			}
+		},
+	},
+}
--- src/screens/Logout/Logout.js
+++ src/screens/Logout/Logout.js
@@ -1,44 +1,40 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import CustomButton from '../../components/CustomButton';
-import {COLORS, FONTS, SIZES} from '../../constants/theme';
-import {useFlusDispatcher} from 'react-flus';
-import {LOGOUT} from '../../flus/constants/auth.const';
-import {useRoute} from '@react-navigation/native';
+import {StyleSheet, Text, View} from 'react-native'
+import React from 'react'
+import CustomButton from '../../components/CustomButton'
+import {COLORS, FONTS, SIZES} from '../../constants/theme'
+import {useFlusDispatcher} from 'react-flus'
+import {LOGOUT} from '../../flus/constants/auth.const'
 
 const Logout = () => {
-  const dispatcher = useFlusDispatcher();
-  const jjjjjjj = useRoute();
+	const dispatcher = useFlusDispatcher()
 
-  console.log({jjjjjjj});
+	return (
+		<View style={styles.container}>
+			<Text style={styles.logoutText}>Logout</Text>
+			<CustomButton
+				title="Logout"
+				onPress={() => {
+					dispatcher({
+						type: LOGOUT,
+					})
+				}}
+			/>
+		</View>
+	)
+}
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.logoutText}>Logout</Text>
-      <CustomButton
-        title="Logout"
-        onPress={() => {
-          dispatcher({
-            type: LOGOUT,
-          });
-        }}
-      />
-    </View>
-  );
-};
-
-export default Logout;
+export default Logout
 
 const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: SIZES.font1,
-    backgroundColor: COLORS.white,
-    flex: 1,
-  },
-  logoutText: {
-    marginTop: 100,
-    marginBottom: 200,
-    alignSelf: 'center',
-    ...FONTS.h3,
-  },
-});
+	container: {
+		paddingHorizontal: SIZES.font1,
+		backgroundColor: COLORS.white,
+		flex: 1,
+	},
+	logoutText: {
+		marginTop: 100,
+		marginBottom: 200,
+		alignSelf: 'center',
+		...FONTS.h3,
+	},
+})
