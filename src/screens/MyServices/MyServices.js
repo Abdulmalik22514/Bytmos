95a93c2d1d2492a7b8efdc33214af94dd2400758
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-07-26 00:15:35 +0100

    Work in progress

--- src/screens/MyServices/MyServices.js
+++ src/screens/MyServices/MyServices.js
@@ -1,9 +1,11 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Header from '../../components/Header';
 
 const MyServices = () => {
   return (
     <View>
+      <Header isNotHome />
       <Text>MyServices</Text>
     </View>
   );
--- src/screens/RecentWorks/RecentWorks.js
+++ src/screens/RecentWorks/RecentWorks.js
@@ -1,11 +1,13 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
+import Header from '../../components/Header';
 
 const RecentWorks = () => {
   const {goBack} = useNavigation();
   return (
     <View>
+      <Header isNotHome />
       <Text onPress={goBack}>RecentWorks</Text>
     </View>
   );
