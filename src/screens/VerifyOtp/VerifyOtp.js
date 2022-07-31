5b4af7063040c15209ac125e416d9de7800bf02d
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-07-31 01:57:07 +0100

    feat: Add verify success message and modal

--- src/screens/VerifyOtp/VerifyOtp.js
+++ src/screens/VerifyOtp/VerifyOtp.js
@@ -1,6 +1,6 @@
-import {Alert, Image, Pressable, Text, View} from 'react-native';
+import {Image, Pressable, Text, View} from 'react-native';
 import React, {useRef, useState} from 'react';
-import {useNavigation, useRoute} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import icons from '../../constants/icons';
 import OTPInputView from '@twotalltotems/react-native-otp-input';
 import CustomButton from '../../components/CustomButton';
@@ -8,11 +8,15 @@ import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import {Formik} from 'formik';
 import * as yup from 'yup';
 import {VerifyOtpStyle as styles} from './styles';
-import {LOGIN_SCREEN} from '../../constants/screens';
+import {ACCOUNT_SCREEN} from '../../constants/screens';
 import {useMutation} from 'react-query';
 import {useAuthApis} from '../../services/api/Auth/auth.index';
 import {resetToken} from '../../services/authServices';
 import {sleep} from './../../utils/utils';
+import Container from '../../components/Container';
+import {CheckMark} from '../../assets/svgs/svg';
+import {FONTS, SIZES} from '../../constants/theme';
+import Modal from 'react-native-modal';
 
 const otpValiditor = yup.object().shape({
   otp: yup.string().required('Otp is required'),
@@ -20,8 +24,14 @@ const otpValiditor = yup.object().shape({
 
 const VerifyOtp = () => {
   const {navigate} = useNavigation();
+  const [verify, setVerify] = useState(false);
+  const [isModalVisible, setModalVisible] = useState(false);
 
   const formRef = useRef();
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
 
   const {VerifyOTP, ResendOTP} = useAuthApis();
   /* verify otp handler */
@@ -30,7 +40,9 @@ const VerifyOtp = () => {
       if (res?.status) {
         resetToken();
 
-        sleep().then(() => navigate(LOGIN_SCREEN));
+        sleep().then(() => {
+          setVerify(true);
+        });
       }
     },
   });
@@ -50,92 +62,153 @@ const VerifyOtp = () => {
   const resendingOtp = resendOTPApi.isLoading;
 
   return (
-    <Formik
-      initialValues={{otp: ''}}
-      validationSchema={otpValiditor}
-      onSubmit={handleOTPVerification}
-      innerRef={formRef}>
-      {({
-        handleChange,
-        handleSubmit,
-        isSubmitting,
-        values,
-        errors,
-        touched,
-        setFieldError,
-      }) => (
-        <>
-          <KeyboardAwareScrollView
-            showsVerticalScrollIndicator={false}
-            contentContainerStyle={styles.container}>
-            <Image
-              source={icons.Verify_Otp}
-              resizeMode="contain"
-              style={styles.image}
-            />
-            <Text style={styles.otpVerify}>Verification</Text>
-            <Text style={styles.verifyText}>
-              Please enter the verification code that was sent to your
-              registered email
-            </Text>
-            <OTPInputView
-              style={styles.otpInputView}
-              pinCount={4}
-              code={values?.otp}
-              onCodeChanged={code => {
-                handleChange('otp')(code);
-              }}
-              autoFocusOnLoad
-              codeInputFieldStyle={[
-                styles.underlineStyleBase,
-                touched.otp && errors.otp && styles.inputErroe,
-              ]}
-              codeInputHighlightStyle={styles.underlineStyleHighLighted}
-            />
-
-            {errors.otp === 'verify-error' && (
-              <View style={{alignItems: 'center'}}>
-                <Text style={styles.invalidCode}>
-                  Verification code invalid
+    <Container>
+      {verify ? (
+        <View style={{alignItems: 'center'}}>
+          <CheckMark style={{marginTop: SIZES.font1 * 4}} />
+          <Text style={[FONTS.body3, {marginTop: SIZES.font10}]}>
+            Verification Successful!
+          </Text>
+          <CustomButton
+            title="Start"
+            onPress={() => {
+              setModalVisible(!isModalVisible);
+            }}
+            style={styles.startButton}
+          />
+        </View>
+      ) : (
+        <Formik
+          initialValues={{otp: ''}}
+          validationSchema={otpValiditor}
+          onSubmit={handleOTPVerification}
+          innerRef={formRef}>
+          {({
+            handleChange,
+            handleSubmit,
+            isSubmitting,
+            values,
+            errors,
+            touched,
+            setFieldError,
+          }) => (
+            <>
+              <KeyboardAwareScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={styles.container}>
+                <Image
+                  source={icons.Verify_Otp}
+                  resizeMode="contain"
+                  style={styles.image}
+                />
+                <Text style={styles.otpVerify}>Verification</Text>
+                <Text style={styles.verifyText}>
+                  Please enter the verification code that was sent to your
+                  registered email
                 </Text>
-                <Pressable onPress={handleResend}>
-                  {resendingOtp && (
-                    <Text style={styles.resendingText}>Resending...</Text>
-                  )}
-                  {!resendingOtp && (
-                    <Text style={styles.resendText}>Resend</Text>
-                  )}
-                </Pressable>
-              </View>
-            )}
+                <OTPInputView
+                  style={styles.otpInputView}
+                  pinCount={4}
+                  code={values?.otp}
+                  onCodeChanged={code => {
+                    handleChange('otp')(code);
+                  }}
+                  autoFocusOnLoad
+                  codeInputFieldStyle={[
+                    styles.underlineStyleBase,
+                    touched.otp && errors.otp && styles.inputErroe,
+                  ]}
+                  codeInputHighlightStyle={styles.underlineStyleHighLighted}
+                />
 
-            {errors.otp !== 'verify-error' && (
-              <View style={styles.resendOtpView}>
-                {resendingOtp && (
-                  <Text style={styles.resendingText}>Resending...</Text>
-                )}
-
-                {!resendingOtp && (
-                  <>
-                    <Text style={styles.noOtpReceive}>Didn’t receive OTP?</Text>
+                {errors.otp === 'verify-error' && (
+                  <View style={{alignItems: 'center'}}>
+                    <Text style={styles.invalidCode}>
+                      Verification code invalid
+                    </Text>
                     <Pressable onPress={handleResend}>
-                      <Text style={styles.resendText}> Resend</Text>
+                      {resendingOtp && (
+                        <Text style={styles.resendingText}>Resending...</Text>
+                      )}
+                      {!resendingOtp && (
+                        <Text style={styles.resendText}>Resend</Text>
+                      )}
                     </Pressable>
-                  </>
+                  </View>
                 )}
-              </View>
-            )}
-            <CustomButton
-              title="Proceed"
-              style={styles.proceedButton}
-              disabled={isLoading || resendingOtp}
-              onPress={handleSubmit}
-              isLoading={isSubmitting || isLoading}
-            />
-          </KeyboardAwareScrollView>
-        </>
+
+                {errors.otp !== 'verify-error' && (
+                  <View style={styles.resendOtpView}>
+                    {resendingOtp && (
+                      <Text style={styles.resendingText}>Resending...</Text>
+                    )}
+
+                    {!resendingOtp && (
+                      <>
+                        <Text style={styles.noOtpReceive}>
+                          Didn’t receive OTP?
+                        </Text>
+                        <Pressable onPress={handleResend}>
+                          <Text style={styles.resendText}> Resend</Text>
+                        </Pressable>
+                      </>
+                    )}
+                  </View>
+                )}
+                <CustomButton
+                  title="Proceed"
+                  style={styles.proceedButton}
+                  disabled={isLoading || resendingOtp}
+                  onPress={handleSubmit}
+                  isLoading={isSubmitting || isLoading}
+                />
+              </KeyboardAwareScrollView>
+            </>
+          )}
+        </Formik>
       )}
-    </Formik>
+      <Modal
+        onBackdropPress={toggleModal}
+        isVisible={isModalVisible}
+        backdropOpacity={0.7}>
+        <View>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>Create a</Text>
+            <CustomButton
+              title="Personal Account"
+              style={styles.modalButton}
+              onPress={() => {
+                toggleModal();
+                //to delay navigation after modal is closed
+                setTimeout(
+                  () =>
+                    navigate(ACCOUNT_SCREEN, {
+                      accountType: 'Personal',
+                    }),
+                  150,
+                );
+              }}
+            />
+            <Text style={{...FONTS.body4, fontSize: 22}}>OR</Text>
+            <CustomButton
+              title="Business Account"
+              style={styles.modalButton}
+              onPress={() => {
+                toggleModal();
+                //to delay navigation after modal is closed
+                setTimeout(
+                  () =>
+                    navigate(ACCOUNT_SCREEN, {
+                      accountType: 'Business',
+                    }),
+                  150,
+                );
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
+    </Container>
   );
 };
 
