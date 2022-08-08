6b845d39e885a3b430f71c174667730b370f7e67
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-08-08 22:54:09 +0100

    chore: refactor components

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,36 +1,27 @@
-import {StyleSheet, Text, TextInput, View} from 'react-native'
-import React from 'react'
-import {COLORS, FONTS, SIZES} from '../constants/theme'
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import React from 'react';
+import {COLORS, FONTS, SIZES} from '../constants/theme';
 
 const InputField = ({label, ...rest}) => {
-	return (
-		<View style={styles.container}>
-			<View style={styles.header}>
-				<Text style={styles.label}>{label}</Text>
-			</View>
-			<TextInput style={[styles.textInput]} {...rest} />
-		</View>
-	)
-}
+  return (
+    <View style={styles.container}>
+      <Text style={FONTS.body4}>{label}</Text>
+      <TextInput style={[styles.textInput]} {...rest} />
+    </View>
+  );
+};
 
-export default InputField
+export default InputField;
 
 const styles = StyleSheet.create({
-	container: {
-		marginVertical: SIZES.font10,
-	},
-	header: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-	},
-	textInput: {
-		...FONTS.body3,
-		borderBottomWidth: 1,
-		borderBottomColor: COLORS.input,
-		padding: SIZES.font10 * 0.5,
-		height: SIZES.font1 * 1.5,
-	},
-	label: {
-		...FONTS.body4,
-	},
-})
+  container: {
+    marginVertical: SIZES.font10,
+  },
+  textInput: {
+    ...FONTS.body3,
+    borderBottomWidth: 1,
+    borderBottomColor: COLORS.input,
+    padding: SIZES.font10 * 0.5,
+    height: SIZES.font1 * 1.5,
+  },
+});
