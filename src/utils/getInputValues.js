8fcf1266e679864c5d01ae4d9aa5a0422c887c01
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-08-10 01:46:48 +0100

    chore: add business input value

--- src/utils/getInputValues.js
+++ src/utils/getInputValues.js
@@ -6,10 +6,29 @@ const INPUT_LIST = [
   {label: 'Name of Business*', key: 'business_name'},
   {label: 'Phone Number*', key: 'phone_number'},
   {label: 'Instagram', key: 'instagram_link'},
+  {label: 'City/State*', key: 'state'},
+  {label: 'Location in this City/State* ', key: 'location'},
+];
+
+const BUSINESS_INPUT_LIST = [
+  {label: 'Registrar First Name*', key: 'first_name'},
+  {label: 'Registrar Last Name*', key: 'last_name'},
+  {label: 'Name of Company*', key: 'company_name'},
+  {label: 'Registrar Position*', key: 'registrar_position'},
+  {label: 'Email of Company*', key: 'email'},
+  {label: 'Phone Number*', key: 'phone_number'},
+  {label: 'Instagram', key: 'instagram_link'},
+  {label: 'City/State*', key: 'state'},
+  {label: 'Company location in this City/State* ', key: 'location'},
+  {label: 'Facebook', key: 'instagram_link'},
 ];
 
 export const getInputValues = (keys = []) => {
   return INPUT_LIST.filter(item => [...keys].includes(item.key));
+};
+
+export const getBusinessInputValues = (keys = []) => {
+  return BUSINESS_INPUT_LIST.filter(item => [...keys].includes(item.key));
 };
 
 export const UPPER_KEYS = [
@@ -18,4 +37,11 @@ export const UPPER_KEYS = [
   'email',
   'business_name',
   'phone_number',
+];
+
+export const BUS_UPPER_KEYS = [
+  'first_name',
+  'last_name',
+  'company_name',
+  'registrar_position',
 ];
