8415f56e0081c98e72f9c79f77b160065146a1bb
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-08-17 01:04:28 +0100

    refactor: add new screens

--- src/navigation/TribeNavigator.js
+++ src/navigation/TribeNavigator.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {NEWPOST_SCREEN, TRIBE_SCREEN} from '../constants/screens';
+import {
+  NEWPOST_SCREEN,
+  TRIBALS_SCREEN,
+  TRIBE_SCREEN,
+} from '../constants/screens';
 import Tribe from '../screens/Tribe/Tribe';
 import NewPost from '../screens/NewPost/NewPost';
+import Tribals from '../screens/Tribals/Tribals';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +19,7 @@ export const TribeNavigator = () => {
       }}>
       <Stack.Screen name={TRIBE_SCREEN} component={Tribe} />
       <Stack.Screen name={NEWPOST_SCREEN} component={NewPost} />
+      <Stack.Screen name={TRIBALS_SCREEN} component={Tribals} />
     </Stack.Navigator>
   );
 };
